b6b02de0bf5c46b237eefe7380689bc9458a6a3b
Bhaveshkatta <bhaveshkatta942@gmail.com>
2024-03-09 14:36:50 +0530

    Implemented Tags and Fixed Images Issue
    Home, Courses and about us tags are implemented and Images scroller is fixed as well

--- script.js
+++ script.js
@@ -63,20 +63,25 @@ gsap.from("#about-us, #about-us-in",{
 })
 
 
-gsap.from(".card",{
-    scale:0.8,
-    opacity:0, 
-    duration:1,
-    stagger:0.1,
-    scrollTrigger:{
-        trigger: ".card",
-        scroller:"body",
-        // markers: true,
-        start:"top 70%",
-        end:"top 65%",
-        scrub:1
-    }
-})
+gsap.registerPlugin(ScrollTrigger);
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  gsap.from(card, {
+    scale: 0.8,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: card,
+      scroller: "body",
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    //   markers: true,
+    },
+  });
+});
 
 
 gsap.from("#colon1",{
